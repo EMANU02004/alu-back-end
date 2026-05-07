f8bac64f0b788385cf03b3155129e28627876d7b
EMANU02004 <e.masambu@alustudent.com>
2026-05-07 11:35:51 +0200

    data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
-import json
+"""For a given employee ID, returns information about TODO list progress."""
+import requests
 import sys
-from urllib.request import urlopen
 
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id
-    )
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        employee_id
-    )
-
-    with urlopen(user_url) as response:
-        user = json.loads(response.read().decode("utf-8"))
-
-    with urlopen(todos_url) as response:
-        todos = json.loads(response.read().decode("utf-8"))
-
-    employee_name = user.get("name")
-    done_tasks = [task for task in todos if task.get("completed") is True]
-
+    employee_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(url + "/users/" + str(employee_id)).json()
+    todos = requests.get(
+        url + "/todos", params={"userId": employee_id}).json()
+    name = user.get("name")
+    done = [t for t in todos if t.get("completed")]
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), len(todos)
-    ))
+        name, len(done), len(todos)))
+    for task in done:
+        print("\t " + task.get("title"))
 
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
