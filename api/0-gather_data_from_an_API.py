51cd8225c02fff5ecf4f240cd484cf8505141e8e
EMANU02004 <e.masambu@alustudent.com>
2026-05-07 10:26:10 +0200

    data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""Module that gathers data from a REST API and displays
-an employee's TODO list progress."""
-
+"""Fetches employee TODO list progress from REST API"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
+    employee_id = sys.argv[1]
 
     base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
 
-    user = requests.get(
-        "{}/users/{}".format(base_url, employee_id)
-    ).json()
-
-    todos = requests.get(
-        "{}/todos?userId={}".format(base_url, employee_id)
-    ).json()
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
 
     employee_name = user.get("name")
-
     done_tasks = [task for task in todos if task.get("completed")]
 
     print("Employee {} is done with tasks({}/{}):".format(
