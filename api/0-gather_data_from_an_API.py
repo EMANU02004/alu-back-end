a2e8c39455b4b587fbfcdca0bac90e9af61ebe91
EMANU02004 <e.masambu@alustudent.com>
2026-05-07 10:44:53 +0200

    data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Gather data from an API"""
-import requests
+import json
 import sys
+from urllib.request import urlopen
 
 
 if __name__ == "__main__":
@@ -10,15 +11,18 @@ if __name__ == "__main__":
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
         employee_id
     )
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
         employee_id
     )
 
-    user = requests.get(user_url).json()
-    todos = requests.get(todos_url).json()
+    with urlopen(user_url) as response:
+        user = json.loads(response.read().decode("utf-8"))
+
+    with urlopen(todos_url) as response:
+        todos = json.loads(response.read().decode("utf-8"))
 
     employee_name = user.get("name")
-    done_tasks = [task for task in todos if task.get("completed")]
+    done_tasks = [task for task in todos if task.get("completed") is True]
 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, len(done_tasks), len(todos)
