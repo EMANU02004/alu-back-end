a7af594febd50a75febe60478a12636465db15b0
Emmanuel Masambu <125499034+EMANU02004@users.noreply.github.com>
2026-06-24 12:40:35 +0200

    initial commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,20 +16,17 @@ def main():
     total_questions = 0
     completed = []
     for todo in response.json():
-
         if todo['userId'] == user_id:
             total_questions += 1
-
             if todo['completed']:
                 completed.append(todo['title'])
 
     user_name = requests.get(user_url).json()['name']
 
-    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
-               len(completed), total_questions))
-    print(printer)
-    for q in completed:
-        print("\t {}".format(q))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name, len(completed), total_questions))
+    for task in completed:
+        print("\t {}".format(task))
 
 
 if __name__ == '__main__':
