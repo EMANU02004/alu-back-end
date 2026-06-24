5670ce684a64a29262a268a7e56cba80e30d641a
Emmanuel Masambu <125499034+EMANU02004@users.noreply.github.com>
2026-06-24 12:35:21 +0200

    initial commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,56 +1,36 @@
 #!/usr/bin/python3
-"""
-Fetch and display an employee's TODO list progress
-from https://jsonplaceholder.typicode.com
-"""
+"""Script to get todos for a user from API"""
 
 import requests
 import sys
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    """main function"""
+    user_id = int(sys.argv[1])
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
+    response = requests.get(todo_url)
 
-    base_url = "https://jsonplaceholder.typicode.com"
+    total_questions = 0
+    completed = []
+    for todo in response.json():
 
-    # Fetch employee info
-    user_resp = requests.get(f"{base_url}/users/{employee_id}")
-    if user_resp.status_code != 200:
-        sys.exit(1)
+        if todo['userId'] == user_id:
+            total_questions += 1
 
-    user = user_resp.json()
-    employee_name = user.get("name")
+            if todo['completed']:
+                completed.append(todo['title'])
 
-    # Fetch todos
-    todos_resp = requests.get(f"\
-    {base_url}/todos", params={"userId": employee_id})
+    user_name = requests.get(user_url).json()['name']
 
-    if todos_resp.status_code != 200:
-        sys.exit(1)
-
-    todos = todos_resp.json()
-
-    total_tasks = len(todos)
-    completed_tasks = [t for t in todos if t.get("completed") is True]
-    done_tasks = len(completed_tasks)
-
-    # Output (EXACT format)
-    print(
-        f"Employee {employee_name} is done "
-        f"with tasks({done_tasks}/{total_tasks}):"
-    )
-
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
+               len(completed), total_questions))
+    print(printer)
+    for q in completed:
+        print("\t {}".format(q))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
