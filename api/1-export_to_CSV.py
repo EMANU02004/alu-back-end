587580c8de12d55d7688777066b44b6cd348e936
EMANU02004 <e.masambu@alustudent.com>
2026-06-24 13:24:05 +0200

    initial

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""Gather data from an API and export employee tasks to CSV format."""
+"""Script to export an employee's TODO list to CSV format."""
+
 import csv
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    base_url = "https://jsonplaceholder.typicode.com"
+def main():
+    """Fetch an employee's tasks and write them to USER_ID.csv."""
+    user_id = int(sys.argv[1])
+    base = 'https://jsonplaceholder.typicode.com'
 
-    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
-    todos = requests.get(
-        "{}/todos".format(base_url), params={"userId": employee_id}
-    ).json()
+    user = requests.get('{}/users/{}'.format(base, user_id)).json()
+    username = user.get('username')
 
-    username = user.get("username")
-    filename = "{}.csv".format(employee_id)
+    todos = requests.get('{}/todos'.format(base),
+                         params={'userId': user_id}).json()
 
-    with open(filename, mode="w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    filename = '{}.csv'.format(user_id)
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow([
-                employee_id,
-                username,
-                task.get("completed"),
-                task.get("title")
-            ])
-        
+            writer.writerow([user_id, username,
+                             task.get('completed'), task.get('title')])
+
+
+if __name__ == '__main__':
+    main()       
